1607ef9427af066e1d22f29fda06faea0aea2d55
Rogue06 <roguenftproject@gmail.com>
2022-11-02 22:33:54 +0100

    terre00.js terminé

--- terre00.js
+++ terre00.js
@@ -1,4 +1,32 @@
-for (let i = 0; i < 10; i += 1) {
-  //j'ai déterminé la répétition sur 10 dans cet exercice.
-  console.log("abcdefghijklmnopqrstuvwxyz \n");
+let tab = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+for (letter of tab) {
+  process.stdout.write(letter);
 }
+console.log("");
