d0dc47485b4344ecc0c396eba304604899611525
cloneMac <roguenftproject@gmail.com>
2022-11-03 23:54:19 +0100

    add instruction on exos and delect one comment

--- terre00.js
+++ terre00.js
@@ -1,3 +1,17 @@
+/**
+ * Alphabet
+ * Créez un programme qui affiche l’alphabet en lettres minuscules suivi d’un retour à la ligne.
+
+
+Exemples d’utilisation :
+$> python exo.py
+abcdefghijklmnopqrstuvwxyz
+$>
+
+
+Attention : votre programme devra utiliser une boucle.
+
+ */
 let tab = [
   "a",
   "b",
--- terre01.js
+++ terre01.js
@@ -1,12 +1,20 @@
-/*const nom = process.argv[0];
-console.log(nom); */
-
-/*const chemin = __filename;
-console.log(chemin); */
-
 /*let test = "Hello World It's Me";
 let stringTab = test.split(" ");
 console.log(stringTab); */
+
+/**
+ * Nom de Programme
+ * Créez un programme qui affiche son nom de fichier.
+
+
+Exemples d’utilisation :
+$> node exo.js
+exo.js
+
+$> node crevette.js
+crevette.js
+
+ */
 
 let chemin = __filename; // je donne à la variable le chemin du fichier
 //let fileName = chemin.split(); //
