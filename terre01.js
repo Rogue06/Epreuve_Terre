5c62494157d066070e1a84614a5a21a818721b4c
cloneMac <roguenftproject@gmail.com>
2022-11-03 23:50:58 +0100

    add terre01.js finish

--- terre01.js
+++ terre01.js
@@ -1,1 +1,13 @@
-console.log("terre01.js");
+/*const nom = process.argv[0];
+console.log(nom); */
+
+/*const chemin = __filename;
+console.log(chemin); */
+
+/*let test = "Hello World It's Me";
+let stringTab = test.split(" ");
+console.log(stringTab); */
+
+let chemin = __filename; // je donne à la variable le chemin du fichier
+//let fileName = chemin.split(); //
+console.log(__filename.slice(__dirname.length + 1));
