0846dd6437265c8b087f7d52d305072cf8bb7095
cloneMac <roguenftproject@gmail.com>
2022-11-05 09:20:06 +0100

    add and finish terre02.js

---
+++ terre02.js
@@ -0,0 +1,25 @@
+/**
+ * AFFICHEUR D'ARGUMENT 
+ * 
+ * Créez un programme qui affiche les arguments qu’il reçoit ligne par ligne, peu importe le nombre d’arguments.
+
+
+Exemples d’utilisation :
+$> ruby exo.rb je suis solide !
+je
+suis
+solide
+!
+
+ */
+
+const process = require("process");
+
+let args = process.argv;
+args.shift(); //permet de supprimer l'index 0
+args.shift(); //permet de supprimer l'index 1
+args.forEach((val) => {
+  console.log(`${val}`);
+});
+
+// ajouter index si on souhaite numéroter les arguments dans le terminal
